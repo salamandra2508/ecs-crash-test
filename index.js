ae3084ed1d6db5e97c7a9ab2d45b7dbc5368dc80
IhorShmidt <ihor.shmidt@techmagic.co>
2022-07-22 00:05:41 +0300

    - of undefined

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -19,12 +21,18 @@ app.use((err, req, res, next) => {
 })
 
 app.get('/value', (req, res, next) => {
-    res.send('value');
+    const value = req.query.value;
+    console.log(`returning value: ${value}`);
+    res.send({value});
 });
 
 app.get('/crash', (req, res, next) => {
-
+    console.log('crushing the server');
     process.exit()
+})
+app.get('/of', (req, res, next) => {
+    const {a} = process.env.NOT_EXISTS;
+    res.send({a});
 })
 
 app.listen(3000, () => {
