1fb70cde76587813e96c3f039fedff98e3b8bf12
IhorShmidt <ihor.shmidt@techmagic.co>
2022-07-22 15:21:06 +0300

    - some improvements

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const badRequest = function(status, error) {
+
+    return {
+        http_code: 400,
+        status: status,
+        error: error || status
+    };
+};
+
 
 app.use((req, res, next) => {
     console.log('req.url: ', req.url);
-    next();
-})
-
-app.use((err, req, res, next) => {
-    console.log('err: ', err);
     next();
 })
 
@@ -24,6 +28,23 @@ app.get('/value', (req, res, next) => {
     const value = req.query.value;
     console.log(`returning value: ${value}`);
     res.send({value});
+});
+
+app.post('/value', (req, res, next) => {
+    console.log('post');
+    const value = req.body.value;
+    console.log(`returning value: ${value}`);
+    res.send({value});
+});
+app.post('/error', (req, res, next) => {
+    console.log('post');
+    const value = req.body.value;
+    console.log(`returning value: ${value}`);
+    try {
+        throw badRequest('invalid_data');
+    } catch (error) {
+        next(error)
+    }
 });
 
 app.get('/crash', (req, res, next) => {
@@ -34,6 +55,19 @@ app.get('/of', (req, res, next) => {
     const {a} = process.env.NOT_EXISTS;
     res.send({a});
 })
+
+
+app.use((err, req, res, next) => {
+    console.log('err: ', err);
+
+    res.status(err.http_code || err.statusCode || err.status || 500).json(err);
+})
+
+app.use(function(req, res) {
+    return res.status(404).send({message: 'Requested route not found.'});
+});
+
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
